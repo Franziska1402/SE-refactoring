2926f6bad324462a13746035fae83c92a1281f60
Franziska1402 <franziskaommer@gmail.com>
2022-05-10 12:20:33 +0200

    test for customer

--- src/tests/CustomerTest.java
+++ src/tests/CustomerTest.java
@@ -1,14 +1,47 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Vector;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import classes.Customer;
+import classes.Movie;
+import classes.Rental;
 
 class CustomerTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	public Customer customer;
+	public Vector rentals;
+
+	@BeforeEach
+	public void beforeEach() {
+		customer = new Customer("Adam");
+		rentals = new Vector();
 	}
 
+	@Test
+	public void getName() {
+		assertEquals("Adam", this.customer.getName());
+	}
+
+	@Test
+	public void statement() {
+		String statementExp = "Rental Record for " + customer.getName() + "\n";
+		statementExp += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
+		statementExp += "Amount owed is " + "0.0" + "\n";
+		statementExp += "You earned " + "0" + " frequent renter points";
+
+		assertEquals(statementExp, customer.statement());
+	}
+
+	@Test
+	public void addRental() {
+		int i = rentals.size();
+
+		rentals.addElement(new Rental(new Movie("TITLE", 0), 0));
+		assertEquals(i + 1, rentals.size());
+	}
 }
