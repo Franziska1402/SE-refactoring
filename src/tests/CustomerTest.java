8d12b34131265fccac6d8fb3a5221546936c0b4e
Franziska1402 <franziskaommer@gmail.com>
2022-05-10 10:43:24 +0200

    added empty test classes

---
+++ src/tests/CustomerTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Service
+ *
+ */
+class CustomerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
